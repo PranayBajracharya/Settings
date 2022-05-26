5ee573236d66fffdddd7f0a166fbf8a971cd1a8d
PranayBajracharya <law.pran99@gmail.com>
2022-05-26 18:17:07 +0545

    file structure

---
+++ lib/settings/settings.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:settings/settings/searchBar/searchBar.dart';
+import 'package:settings/settings/settingsList/settingsList.dart';
+
+class Settings extends StatelessWidget {
+  const Settings({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Settings"),
+        backgroundColor: Colors.black,
+      ),
+      body: Column(
+        children: [
+          SearchBar(),
+          SettingsList(),
+        ],
+      ),
+    );
+  }
+}
